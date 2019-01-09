9bc7fdefd315e76923da4e307e812249890a8298
Anthony Gregg <agregg22@gmail.com>
2019-01-09 00:31:43 -0500

    Complete

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+end
+
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+end
+
+
+
+class Dog
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
